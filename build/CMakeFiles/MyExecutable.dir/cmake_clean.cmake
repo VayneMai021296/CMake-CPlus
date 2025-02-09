ee1346ed74f89394ddf17b2993fead138d8606bb
Vayne <manmuoi021204@gmail.com>
2025-02-09 13:34:53 +0700

    add virtual_destructor and std_unique_ptr

---
+++ build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/SourcesFile/std_unique_ptr.cpp.o"
+  "CMakeFiles/MyExecutable.dir/SourcesFile/std_unique_ptr.cpp.o.d"
+  "CMakeFiles/MyExecutable.dir/SourcesFile/virtual_destructor.cpp.o"
+  "CMakeFiles/MyExecutable.dir/SourcesFile/virtual_destructor.cpp.o.d"
+  "CMakeFiles/MyExecutable.dir/main.cpp.o"
+  "CMakeFiles/MyExecutable.dir/main.cpp.o.d"
+  "MyExecutable"
+  "MyExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
