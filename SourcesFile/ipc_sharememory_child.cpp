2b5caaaed1299241fb0f5169d4042f16d8f33c2e
Vayne <manmuoi021204@gmail.com>
2025-03-01 13:26:50 +0700

    update ipc

--- SourcesFile/ipc_sharememory_child.cpp
+++ SourcesFile/ipc_sharememory_child.cpp
@@ -3,13 +3,16 @@
 #include <sys/shm.h>
 
 int main() {
-    key_t key = ftok("shmfile", 65);
-    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
-    char *str = (char*) shmat(shmid, nullptr, 0);
-
-    std::cout << "Data read: " << str << std::endl;
-    
-    shmdt(str);
-    shmctl(shmid, IPC_RMID, nullptr); // Xóa shared memory
-    return 0;
+    while (true)
+    {
+        key_t key = ftok("shmfile", 65);
+        int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+        char *str = (char*) shmat(shmid, nullptr, 0);
+       
+        std::cout << "Data read: " << str << std::endl;
+        
+        shmdt(str);
+        shmctl(shmid, IPC_RMID, nullptr); // Xóa shared memory
+        /* code */
+    }
 }
--- SourcesFile/ipc_sharememory_parent.cpp
+++ SourcesFile/ipc_sharememory_parent.cpp
@@ -5,12 +5,18 @@
 
 int main() {
     key_t key = ftok("shmfile", 65);
-    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
-    char *str = (char*) shmat(shmid, nullptr, 0);
+    std::string input_value ;
+    while (true)
+    {
+        std::cout << "Please Enter the string you want to send: "<< std::endl;
+        std::cin >> input_value;
+        int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+        char *str = (char*) shmat(shmid, nullptr, 0);
 
-    strcpy(str, "Shared Memory IPC");
-    std::cout << "Data written: " << str << std::endl;
+        strcpy(str, "Shared Memory IPC");
+        std::cout << "Data written: " << str << std::endl;
 
-    shmdt(str);
-    return 0;
+        shmdt(str);
+        /* code */
+    }
 }
